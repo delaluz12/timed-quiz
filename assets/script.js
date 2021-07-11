41d8d0d17eead39dcd4aa60a2626dcf3bfd66139
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-07-11 11:59:32 -0500

    added all project files and pseudo code

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+//Psuedo Code 
+
+//Taking a timed code quiz - I am presented with a welcome screen with instructions
+
+//Once I am ready to start game I will click the start button
+
+//When I click the start button welcome screen disappears
+    //Timer will begin
+    //I am presented with first question 
+        //If I answer correctly then 1pt will be added to score & I will be presented with the next question
+        //If I answer incorrectly I will be deducted time from the timer & then be presented with the next question
+    //When all questions are answered/cycled through or timer reaches zero 
+    //Then game is over
+    //When game is over then I am presented with input for initals & my score from correctly        answered questions
+    //Once I enter my initals I click the save button to save initals and score
+    //Then I am presented with my initials with score and two buttons
+        //button to "go back" to the welcome screen to restart game
+        //button to clear all scores
